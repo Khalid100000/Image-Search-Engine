3da5d3ec50f15878f59c1008e2917e890d2a8df1
Khalid100000 <102392815+Khalid100000@users.noreply.github.com>
2022-06-20 04:32:56 +0200

    adding the class

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 import numpy as np
 from PIL import Image
-from feature_extractor import FeatureExtractor
+#from feature_extractor import FeatureExtractor
 from datetime import datetime
 from flask import Flask, request, render_template
 from pathlib import Path
 import cv2
-
+class FeatureExtractor:
+	def __init__(self):
+		self.model=cv2.xfeatures2d.SIFT_create()
+		#self.FLANN_INDEX_KDTREE=1
+		self.index_params=dict(algorithm=1,trees=5)
+		self.search_params=dict(checks=50)   # or pass empty dictionary
+		self.flann=cv2.FlannBasedMatcher(self.index_params,self.search_params)
+	def extract(self, img):
+		"""
+		Extract a deep feature from an input image
+		Args:
+			img: a gray scale image from cv2.imread
+			Returns:
+			feature (np.ndarray): deep feature with the shape=(#p,128)
+		"""
+		_,feature = self.model.detectAndCompute(img,None)  # (1, 4096) -> (4096, )
+		return feature
+	def matching(self,des1,des2):
+		matches=self.flann.knnMatch(des1,des2,k=2)
+		N_matches=sum([1 for m,n in matches if m.distance < 0.7*n.distance])
+		if N_matches == 0:
+			return 1000
+		else:
+   			return 1.0/N_matches*1000
 app = Flask(__name__)
 # Read image features
 fe = FeatureExtractor()
@@ -17,7 +40,7 @@ for feature_path in Path("./static/feature").glob("*.npy"):
 #features = np.array(features)
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['query_img']
